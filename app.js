add75e37ab985b4a87b0cd4d4212cbd3a607f57f
Deepika Agrawal <144984874+DeepikaaAgrawal@users.noreply.github.com>
2025-01-14 19:57:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.use((req,res,next)=>{
 
 //app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
-app.use("/listings",listingRouter);
+app.use("/",listingRouter);
 //app.use("/listings",userRouter);
 
 
